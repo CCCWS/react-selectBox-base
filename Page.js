1623cd51a53d65a1c1b911a4f64d017850f5f522
CCCWS <86645532+CCCWS@users.noreply.github.com>
2022-06-17 21:03:30 +0900

    Create Page.js

---
+++ Page.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import SelectBox from "../Components/SelectBox";
+
+function Page() {
+  const [selectItem, setSelectItem] = useState("카테고리 선택");
+  return (
+    <div>
+      <SelectBox
+        setValue={setSelectItem}
+        value={selectItem}
+      />
+    </div>
+  );
+}
+
+export default Page;
